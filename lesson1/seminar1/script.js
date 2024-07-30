4d1bba2b1cb3f6829a2d8afee74aee8ce5f872d1
malewin <139165145+malewin@users.noreply.github.com>
2024-07-30 18:24:47 +0600

    homework1 and homework2 are ready

---
+++ lesson1/seminar1/script.js
@@ -0,0 +1,13 @@
+let c = 10;
+let d = 2;
+let sum = c + d;
+let dif = c - d;
+let multify = c * d;
+let division = c / d;
+
+console.log(sum);
+console.log(dif);
+console.log(multify);
+console.log(division);
+
+console.log(sum + dif + multify + division);
---
+++ test/script.js
@@ -0,0 +1,6 @@
+document.querySelector(".button").onclick = myClick();
+
+function myClick(){
+    let b = document.getElementById('input');
+    console.log(b.value);
+}
