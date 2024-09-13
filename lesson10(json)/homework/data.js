a4720531695b53d4abd17e1cab7ec5bb5f947d46
malewin <139165145+malewin@users.noreply.github.com>
2024-09-13 11:25:38 +0600

    homework10 is completed

---
+++ lesson10(json)/homework/data.js
@@ -0,0 +1,28 @@
+const dataInfo = `[{
+        "id": "minter.ton",
+        "collection":"TON DNS COLLECTION",
+        "url": "https://cache.tonapi.io/imgproxy/VaxsVW1pgu5URbczvVXzsTxxgfkjuYu4Ii2fofk-GWI/rs:fill:1500:1500:1/g:no/aHR0cHM6Ly9jYWNoZS50b25hcGkuaW8vZG5zL3ByZXZpZXcvbWludGVyLnRvbi5wbmc.webp",
+        "price":"1000 toncoin",
+        "marketHref":"https://getgems.io/collection/EQC3dNlesgVD8YbAazcauIrXBPfiVhMMr5YYk2in0Mtsz0Bz/EQBXJLmZzTFZ9YxCIF4RlmJrBdPhBNXF8GuJJ3Z9EJgPtQ0a",
+        "width": "150px",
+        "height": "150px"
+    },
+    {
+        "id": "song.ton",
+        "collection":"TON DNS COLLECTION",
+        "url":"https://cache.tonapi.io/imgproxy/NBYxkpePkGPjb-1TYroyh9QBIyYOrk5AWsm1SUgLsrc/rs:fill:1500:1500:1/g:no/aHR0cHM6Ly9jYWNoZS50b25hcGkuaW8vZG5zL3ByZXZpZXcvc29uZy50b24ucG5n.webp",
+        "price":"3000 toncoin",
+        "marketHref":"https://getgems.io/collection/EQC3dNlesgVD8YbAazcauIrXBPfiVhMMr5YYk2in0Mtsz0Bz/EQBDq7gPoegr6937xJbToqYG0BBI4Q4qz5ckwGywD7FM3vj-",
+        "width": "150px",
+        "height": "150px"
+    },
+    {
+        "id": "buffer.ton",
+        "collection":"TON DNS COLLECTION",
+        "url": "https://cache.tonapi.io/imgproxy/AKS6sD_HbtXKw43jXXfBCbTW22lGi3fazFTB1Ff0YeM/rs:fill:1500:1500:1/g:no/aHR0cHM6Ly9jYWNoZS50b25hcGkuaW8vZG5zL3ByZXZpZXcvYnVmZmVyLnRvbi5wbmc.webp",
+        "price":"600 toncoin",
+        "marketHref":"https://getgems.io/collection/EQC3dNlesgVD8YbAazcauIrXBPfiVhMMr5YYk2in0Mtsz0Bz/EQBCYYig6li-O86XwbgmUwjJt8ehkdCSAz_qPyn_ePJuSrgH",
+        "width": "150px",
+        "height": "150px"
+    }
+]`
